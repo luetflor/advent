7f0efb21d08f970589b725432e90296d45df93bd
Florian Lütkewitte <florian@luetkewitte.net>
2025-12-11 22:27:54 +0100

    Added Day 2, part two

--- src/day2.rs
+++ src/day2.rs
@@ -7,6 +7,7 @@ struct IDRange {
 
 pub fn day2() {
     part_one();
+    part_two();
 }
 
 fn part_one() {
@@ -29,15 +30,56 @@ fn part_one() {
     println!("Cumulative sum is {cumsum}");
 }
 
+fn part_two() {
+    let ranges = parse_ids("data/day2");
+    let mut cumsum = 0;
+    let mut n = 0;
+    for r in ranges {
+        'idloop: for id in r.start..r.end {
+            let s = id.to_string();
+            for i in 1..s.len() {
+                let repstr = find_repeating_string(&s, i);
+                match  find_repeating_string(&s, i) {
+                    Some(x) => {
+                        cumsum += id;
+                        n += 1;
+                        println!("{s}");
+                        println!("{}", x);
+                        continue 'idloop;
+                    },
+                    None => ()
+                };
+            }
+        }
+    }
+    println!("Found {n} invalid IDs");
+    println!("Cumulative sum is {cumsum}");
+}
+
+fn find_repeating_string(str: &str, freq: usize) -> Option<&str> {
+    if str.len() % freq != 0 {
+        return None;
+    }
+    let first = &str[..freq];
+    // println!("{first}");
+    for i in 1..str.len()/freq {
+        // println!("{}", &str[i*freq..(i+1)*freq]);
+        if &str[i*freq..(i+1)*freq] != first {
+            return None;
+        }
+    }
+    Some(first)
+}
+
 fn parse_ids(filename: &str) -> Vec<IDRange> {
     fs::read_to_string(filename)
         .expect("File not found!")
         .split(",")
         .map( |s| {
-            println!("{s}");
+            // println!("{s}");
             let spl: Vec<&str> = s.split("-").collect();
-            println!("{}", spl[0]);
-            println!("{}", spl[1]);
+            // println!("{}", spl[0]);
+            // println!("{}", spl[1]);
             IDRange {
                 start: spl[0].trim().parse::<u64>()
                     .expect("Not a number"),
@@ -45,4 +87,19 @@ fn parse_ids(filename: &str) -> Vec<IDRange> {
                     .expect("Not a number")
             }
         }).collect()
+}
+
+
+#[test]
+fn repeating_string_test() {
+    let val = "121212";
+    assert_eq!(find_repeating_string(val, 2), Some("12"));
+    let val = "222222222";
+    assert_eq!(find_repeating_string(val, 1), Some("2"));
+    let val = "123456789123456789";
+    assert_eq!(find_repeating_string(val, 9), Some("123456789"));
+    let val = "12345";
+    for i in 1..3 {
+        assert_eq!(find_repeating_string(val, i), None);
+    }
 }
